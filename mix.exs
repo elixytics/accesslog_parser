6ada157487ca3cdcbf3983282cf04442e18fad01
Marc Neudert <marc.neudert@gmail.com>
2017-04-23 15:36:11 +0200

    initializes travis environment

--- mix.exs
+++ mix.exs
@@ -5,8 +5,21 @@ defmodule AccessLogParser.Mixfile do
     [app:     :accesslog_parser,
      version: "0.1.0-dev",
      elixir:  "~> 1.4",
+     deps:    deps(),
 
      build_embedded:  Mix.env == :prod,
-     start_permanent: Mix.env == :prod]
+     start_permanent: Mix.env == :prod,
+
+     preferred_cli_env: [
+       coveralls:          :test,
+       'coveralls.detail': :test,
+       'coveralls.travis': :test
+     ],
+
+     test_coverage: [ tool: ExCoveralls ]]
+  end
+
+  defp deps do
+    [{ :excoveralls, "~> 0.6", only: :test }]
   end
 end
