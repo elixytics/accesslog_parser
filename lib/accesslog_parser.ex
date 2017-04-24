4ebf79f091fa627609d51848f8084128fedd3554
Marc Neudert <marc.neudert@gmail.com>
2017-04-24 18:35:06 +0200

    parses :common_vhost log format

--- lib/accesslog_parser.ex
+++ lib/accesslog_parser.ex
@@ -3,7 +3,11 @@ defmodule AccessLogParser do
   Parses access log lines.
   """
 
-  @re_common ~r/(?P<ip>\S+)\s+\S+\s+(?P<userid>\S+)\s+\[(?P<date>.*?)\s+(?P<timezone>.*?)\]\s+"\S+\s+(?P<path>.*?)\s+\S+"\s+(?P<status>\S+)\s+(?P<length>\S+)/
+  @tpl_common ~S/(?P<ip>\S+)\s+\S+\s+(?P<userid>\S+)\s+\[(?P<date>.*?)\s+(?P<timezone>.*?)\]\s+"\S+\s+(?P<path>.*?)\s+\S+"\s+(?P<status>\S+)\s+(?P<length>\S+)/
+  @tpl_vhost  ~S/(?P<host>[\w\-\.]*)(?::\d+)?\s+/
+
+  @re_common       Regex.compile!(@tpl_common)
+  @re_common_vhost Regex.compile!(@tpl_vhost <> @tpl_common)
 
   @doc """
   Converts a given line into a map matching the defined format.
@@ -20,7 +24,20 @@ defmodule AccessLogParser do
         "timezone" => "+0200",
         "userid"   => "someuser"
       }
+
+      iex> parse(~s(www.example.com 1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765), :common_vhost)
+      %{
+        "date"     => "22/Apr/2017:15:17:39",
+        "host"     => "www.example.com",
+        "ip"       => "1.2.3.4",
+        "length"   => "765",
+        "path"     => "/",
+        "status"   => "200",
+        "timezone" => "+0200",
+        "userid"   => "someuser"
+      }
   """
   @spec parse(String.t, atom) :: map
-  def parse(line, :common), do: Regex.named_captures(@re_common, line)
+  def parse(line, :common),       do: Regex.named_captures(@re_common, line)
+  def parse(line, :common_vhost), do: Regex.named_captures(@re_common_vhost, line)
 end
