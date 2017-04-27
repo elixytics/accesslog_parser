57f0b6fa0ca7ee49f839e9a6d1941ca9a13fc12c
Marc Neudert <marc.neudert@gmail.com>
2017-04-27 18:26:59 +0200

    configures hex packaging

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule AccessLogParser.Mixfile do
   use Mix.Project
 
+  @url_github "https://github.com/elixytics/accesslog_parser"
+
   def project do
     [app:     :accesslog_parser,
      version: "0.1.0-dev",
@@ -16,10 +18,28 @@ defmodule AccessLogParser.Mixfile do
        'coveralls.travis': :test
      ],
 
+     description:   "Access Log Parser",
+     docs:          docs(),
+     package:       package(),
      test_coverage: [ tool: ExCoveralls ]]
   end
 
   defp deps do
-    [{ :excoveralls, "~> 0.6", only: :test }]
+    [{ :ex_doc,      ">= 0.0.0", only: :dev },
+     { :excoveralls, "~> 0.6",   only: :test }]
+  end
+
+  defp docs do
+    [extras:     [ "CHANGELOG.md", "README.md" ],
+     main:       "readme",
+     source_ref: "master",
+     source_url: @url_github]
+  end
+
+  defp package do
+    %{files:       [ "CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib" ],
+      licenses:    [ "Apache 2.0" ],
+      links:       %{ "GitHub" => @url_github },
+      maintainers: [ "Marc Neudert" ]}
   end
 end
