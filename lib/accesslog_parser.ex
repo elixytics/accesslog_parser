0eca1c498cb13d431919a161f65725c1f0abfb84
Marc Neudert <marc.neudert@gmail.com>
2017-10-11 10:19:42 +0200

    performs initial "mix format"

--- lib/accesslog_parser.ex
+++ lib/accesslog_parser.ex
@@ -5,12 +5,12 @@ defmodule AccessLogParser do
 
   @tpl_client ~S/\s+"(?P<referrer>.*?)"\s+"(?P<user_agent>.*?)"/
   @tpl_common ~S/(?P<ip>\S+)\s+\S+\s+(?P<userid>\S+)\s+\[(?P<date>.*?)\s+(?P<timezone>.*?)\]\s+"\S+\s+(?P<path>.*?)\s+\S+"\s+(?P<status>\S+)\s+(?P<length>\S+)/
-  @tpl_vhost  ~S/(?P<host>[\w\-\.]*)(?::\d+)?\s+/
+  @tpl_vhost ~S/(?P<host>[\w\-\.]*)(?::\d+)?\s+/
 
-  @re_common          Regex.compile!(@tpl_common)
+  @re_common Regex.compile!(@tpl_common)
   @re_common_complete Regex.compile!(@tpl_vhost <> @tpl_common <> @tpl_client)
-  @re_common_vhost    Regex.compile!(@tpl_vhost <> @tpl_common)
-  @re_extended        Regex.compile!(@tpl_common <> @tpl_client)
+  @re_common_vhost Regex.compile!(@tpl_vhost <> @tpl_common)
+  @re_extended Regex.compile!(@tpl_common <> @tpl_client)
 
   @doc """
   Converts a given line into a map matching the defined format.
@@ -67,9 +67,9 @@ defmodule AccessLogParser do
         "userid"     => "someuser"
       }
   """
-  @spec parse(String.t, atom) :: map
-  def parse(line, :common),          do: Regex.named_captures(@re_common, line)
+  @spec parse(String.t(), atom) :: map
+  def parse(line, :common), do: Regex.named_captures(@re_common, line)
   def parse(line, :common_complete), do: Regex.named_captures(@re_common_complete, line)
-  def parse(line, :common_vhost),    do: Regex.named_captures(@re_common_vhost, line)
-  def parse(line, :extended),        do: Regex.named_captures(@re_extended, line)
+  def parse(line, :common_vhost), do: Regex.named_captures(@re_common_vhost, line)
+  def parse(line, :extended), do: Regex.named_captures(@re_extended, line)
 end
--- mix.exs
+++ mix.exs
@@ -4,42 +4,47 @@ defmodule AccessLogParser.Mixfile do
   @url_github "https://github.com/elixytics/accesslog_parser"
 
   def project do
-    [app:     :accesslog_parser,
-     version: "0.2.0-dev",
-     elixir:  "~> 1.4",
-     deps:    deps(),
-
-     build_embedded:  Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-
-     preferred_cli_env: [
-       coveralls:          :test,
-       'coveralls.detail': :test,
-       'coveralls.travis': :test
-     ],
-
-     description:   "Access Log Parser",
-     docs:          docs(),
-     package:       package(),
-     test_coverage: [ tool: ExCoveralls ]]
+    [
+      app: :accesslog_parser,
+      version: "0.2.0-dev",
+      elixir: "~> 1.4",
+      deps: deps(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.travis": :test
+      ],
+      description: "Access Log Parser",
+      docs: docs(),
+      package: package(),
+      test_coverage: [tool: ExCoveralls]
+    ]
   end
 
   defp deps do
-    [{ :ex_doc,      ">= 0.0.0", only: :dev },
-     { :excoveralls, "~> 0.7",   only: :test }]
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:excoveralls, "~> 0.7", only: :test}
+    ]
   end
 
   defp docs do
-    [extras:     [ "CHANGELOG.md", "README.md" ],
-     main:       "readme",
-     source_ref: "master",
-     source_url: @url_github]
+    [
+      extras: ["CHANGELOG.md", "README.md"],
+      main: "readme",
+      source_ref: "master",
+      source_url: @url_github
+    ]
   end
 
   defp package do
-    %{files:       [ "CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib" ],
-      licenses:    [ "Apache 2.0" ],
-      links:       %{ "GitHub" => @url_github },
-      maintainers: [ "Marc Neudert" ]}
+    %{
+      files: ["CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @url_github},
+      maintainers: ["Marc Neudert"]
+    }
   end
 end
