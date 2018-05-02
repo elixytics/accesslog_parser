c461f81254e4d84546a3435e70a29fbbd7d13fef
Marc Neudert <marc.neudert@gmail.com>
2018-05-02 18:09:57 +0200

    replaces doctests with regular tests

--- lib/accesslog_parser.ex
+++ lib/accesslog_parser.ex
@@ -31,58 +31,6 @@ defmodule AccessLogParser do
 
   @doc """
   Converts a given line into a map matching the defined format.
-
-  ## Examples
-
-      iex> parse(~s(1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765), :common)
-      %{
-        "date"     => "22/Apr/2017:15:17:39",
-        "ip"       => "1.2.3.4",
-        "length"   => "765",
-        "path"     => "/",
-        "status"   => "200",
-        "timezone" => "+0200",
-        "userid"   => "someuser"
-      }
-
-      iex> parse(~s(www.example.com 1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765), :common_vhost)
-      %{
-        "date"     => "22/Apr/2017:15:17:39",
-        "host"     => "www.example.com",
-        "ip"       => "1.2.3.4",
-        "length"   => "765",
-        "path"     => "/",
-        "status"   => "200",
-        "timezone" => "+0200",
-        "userid"   => "someuser"
-      }
-
-      iex> parse(~s(1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765 "-" "Mozilla/5.0 \\(X11; Linux x86_64\\) AppleWebKit/535.11 \\(KHTML, like Gecko\\) Chrome/17.0.963.56 Safari/535.11"), :extended)
-      %{
-        "date"       => "22/Apr/2017:15:17:39",
-        "ip"         => "1.2.3.4",
-        "length"     => "765",
-        "path"       => "/",
-        "referrer"   => "-",
-        "status"     => "200",
-        "timezone"   => "+0200",
-        "user_agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-        "userid"     => "someuser"
-      }
-
-      iex> parse(~s(www.example.com 1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765 "-" "Mozilla/5.0 \\(X11; Linux x86_64\\) AppleWebKit/535.11 \\(KHTML, like Gecko\\) Chrome/17.0.963.56 Safari/535.11"), :common_complete)
-      %{
-        "date"       => "22/Apr/2017:15:17:39",
-        "host"       => "www.example.com",
-        "ip"         => "1.2.3.4",
-        "length"     => "765",
-        "path"       => "/",
-        "referrer"   => "-",
-        "status"     => "200",
-        "timezone"   => "+0200",
-        "user_agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-        "userid"     => "someuser"
-      }
   """
   @spec parse(String.t(), atom) :: map
   def parse(line, :common), do: Regex.named_captures(@re_common, line)
--- test/accesslog_parser_test.exs
+++ test/accesslog_parser_test.exs
@@ -1,5 +1,75 @@
 defmodule AccessLogParserTest do
   use ExUnit.Case, async: true
 
-  doctest AccessLogParser, import: true
+  test ":common" do
+    assert AccessLogParser.parse(
+             ~s(1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765),
+             :common
+           ) ==
+             %{
+               "date" => "22/Apr/2017:15:17:39",
+               "ip" => "1.2.3.4",
+               "length" => "765",
+               "path" => "/",
+               "status" => "200",
+               "timezone" => "+0200",
+               "userid" => "someuser"
+             }
+  end
+
+  test ":common_complete" do
+    assert AccessLogParser.parse(
+             ~s(www.example.com 1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765 "-" "Mozilla/5.0 \(X11; Linux x86_64\) AppleWebKit/535.11 \(KHTML, like Gecko\) Chrome/17.0.963.56 Safari/535.11"),
+             :common_complete
+           ) ==
+             %{
+               "date" => "22/Apr/2017:15:17:39",
+               "host" => "www.example.com",
+               "ip" => "1.2.3.4",
+               "length" => "765",
+               "path" => "/",
+               "referrer" => "-",
+               "status" => "200",
+               "timezone" => "+0200",
+               "user_agent" =>
+                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+               "userid" => "someuser"
+             }
+  end
+
+  test ":common_vhost" do
+    assert AccessLogParser.parse(
+             ~s(www.example.com 1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765),
+             :common_vhost
+           ) ==
+             %{
+               "date" => "22/Apr/2017:15:17:39",
+               "host" => "www.example.com",
+               "ip" => "1.2.3.4",
+               "length" => "765",
+               "path" => "/",
+               "status" => "200",
+               "timezone" => "+0200",
+               "userid" => "someuser"
+             }
+  end
+
+  test ":extended" do
+    assert AccessLogParser.parse(
+             ~s(1.2.3.4 - someuser [22/Apr/2017:15:17:39 +0200] "GET / HTTP/1.0" 200 765 "-" "Mozilla/5.0 \(X11; Linux x86_64\) AppleWebKit/535.11 \(KHTML, like Gecko\) Chrome/17.0.963.56 Safari/535.11"),
+             :extended
+           ) ==
+             %{
+               "date" => "22/Apr/2017:15:17:39",
+               "ip" => "1.2.3.4",
+               "length" => "765",
+               "path" => "/",
+               "referrer" => "-",
+               "status" => "200",
+               "timezone" => "+0200",
+               "user_agent" =>
+                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+               "userid" => "someuser"
+             }
+  end
 end
