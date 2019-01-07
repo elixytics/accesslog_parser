1b1e1c2e2adec7cb0b8ae35396afda68da27f32a
Marc Neudert <marc.neudert@gmail.com>
2019-01-07 20:03:27 +0100

    adds :dialyxir dependency

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule AccessLogParser.Mixfile do
   defp deps do
     [
       {:credo, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.8", only: :test, runtime: false}
     ]
