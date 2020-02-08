a427d363f24ff0e3f54dbc931aff21c004d5d60f
Marc Neudert <marc.neudert@gmail.com>
2020-02-08 12:56:25 +0100

    Separate :common parser into concatenated parts

--- lib/accesslog_parser/parsec.ex
+++ lib/accesslog_parser/parsec.ex
@@ -3,28 +3,36 @@ defmodule AccessLogParser.Parsec do
 
   import NimbleParsec
 
+  date = repeat(ascii_string([?a..?z, ?A..?Z, ?0..?9, ?:, ?/], 20))
+  ip = repeat(ascii_string([?a..?f, ?A..?F, ?0..?9, ?:, ?.], min: 7))
+  length = repeat(integer(min: 1))
+  path = repeat(ascii_string([not: 32], min: 1))
+  status = repeat(integer(min: 1))
+  timezone = repeat(ascii_string([?0..?9, ?+, ?-], 5))
+  userid = repeat(ascii_string([not: 32], min: 1))
+
   common =
-    repeat(ascii_string([?a..?f, ?A..?F, ?0..?9, ?:, ?.], min: 7))
+    ip
     |> ignore(ascii_char([32]))
     |> ignore(repeat(ascii_string([not: 32], min: 1)))
     |> ignore(ascii_char([32]))
-    |> repeat(ascii_string([not: 32], min: 1))
+    |> concat(userid)
     |> ignore(ascii_char([32]))
     |> ignore(ascii_char([?[]))
-    |> repeat(ascii_string([?a..?z, ?A..?Z, ?0..?9, ?:, ?/], 20))
+    |> concat(date)
     |> ignore(ascii_char([32]))
-    |> repeat(ascii_string([?0..?9, ?+, ?-], 5))
+    |> concat(timezone)
     |> ignore(ascii_char([?]]))
     |> ignore(ascii_char([32]))
     |> ignore(repeat(ascii_string([not: 32], min: 1)))
     |> ignore(ascii_char([32]))
-    |> repeat(ascii_string([not: 32], min: 1))
+    |> concat(path)
     |> ignore(repeat(ascii_string([not: ?"], min: 1)))
     |> ignore(ascii_char([?"]))
     |> ignore(ascii_char([32]))
-    |> repeat(integer(min: 1))
+    |> concat(status)
     |> ignore(ascii_char([32]))
-    |> repeat(integer(min: 1))
+    |> concat(length)
 
   defparsec :common, common, inline: true
 end
