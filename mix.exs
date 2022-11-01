092341832a8d258e7e0f69b85d01e7341ef4aabd
Marc Neudert <marc.neudert@gmail.com>
2022-11-01 10:50:43 +0100

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule AccessLogParser.MixProject do
       {:credo, "~> 1.6", only: :dev, runtime: false},
       {:dialyxir, "~> 1.2", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
+      {:excoveralls, "~> 0.15.0", only: :test, runtime: false},
       {:nimble_parsec, "~> 1.0"}
     ]
   end
